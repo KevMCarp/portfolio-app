58d18ef1db4c6a532b4df2586e79e36524c2d8eb
Kevin McDermott-Carpenter <kevin.mcdermott-carpenter@concentechltd.co.uk>
2022-05-25 16:51:43 +0100

    switch to future provider

--- lib/screens/status_screen.dart
+++ lib/screens/status_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:service_status/screens/status_widgets/recieved.dart';
 
 import '../components/scaffold.dart';
 import '../models/vehicle_status.dart';
 import '../services/vehicle_status_provider.dart';
 import 'status_widgets/booked.dart';
+import 'status_widgets/recieved.dart';
+
+const _errorText = Text('We could not find the status of your vehicle');
 
 class StatusScreen extends StatelessWidget {
   const StatusScreen({Key? key}) : super(key: key);
@@ -15,24 +17,34 @@ class StatusScreen extends StatelessWidget {
     return AppScaffold(
       child: Center(
         child: Consumer(builder: (context, ref, child) {
-          final vehicleStatus = ref.watch(vehicleStatusProvider);
+          final futureVehicleStatus = ref.watch(vehicleStatusProvider);
 
-          switch (vehicleStatus) {
-            case VehicleStatus.booked:
-              return BookedScreen(
-                timestamp: DateTime.now(),
-              );
-            case VehicleStatus.recieved:
-              return RecievedScreen(
-                timestamp: DateTime.now(),
-              );
-            case VehicleStatus.workshop:
-            case VehicleStatus.ready:
-            case VehicleStatus.collected:
-              return Container();
-            case VehicleStatus.unknown:
-              return Text('We could not find the status of your vehicle');
-          }
+          return futureVehicleStatus.when(
+            loading: () {
+              return const CircularProgressIndicator();
+            },
+            error: (e, _) {
+              return _errorText;
+            },
+            data: (vehicleStatus) {
+              switch (vehicleStatus) {
+                case VehicleStatus.booked:
+                  return BookedScreen(
+                    timestamp: DateTime.now(),
+                  );
+                case VehicleStatus.recieved:
+                  return RecievedScreen(
+                    timestamp: DateTime.now(),
+                  );
+                case VehicleStatus.workshop:
+                case VehicleStatus.ready:
+                case VehicleStatus.collected:
+                  return Container();
+                case VehicleStatus.unknown:
+                  return _errorText;
+              }
+            },
+          );
         }),
       ),
     );
--- lib/services/vehicle_status_provider.dart
+++ lib/services/vehicle_status_provider.dart
@@ -1,5 +1,14 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:service_status/models/vehicle_status.dart';
+import 'dart:math';
 
-final vehicleStatusProvider =
-    StateProvider.autoDispose((ref) => VehicleStatus.booked);
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/vehicle_status.dart';
+import '../utils/extension_methods.dart';
+
+final vehicleStatusProvider = FutureProvider.autoDispose((ref) async {
+  await Future.delayed(1.seconds);
+
+  final random = Random.secure();
+  const values = VehicleStatus.values;
+
+  return values[random.nextInt(values.length - 1)];
+});
