a99e305cffde41498fd139ad3618ed7f39e65891
Kevin McDermott-Carpenter <kevin.mcdermott-carpenter@concentechltd.co.uk>
2022-05-25 17:15:11 +0100

    use future provider randomly return a vehicle status

--- lib/services/vehicle_status_provider.dart
+++ lib/services/vehicle_status_provider.dart
@@ -4,11 +4,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/vehicle_status.dart';
 import '../utils/extension_methods.dart';
 
-final vehicleStatusProvider = FutureProvider.autoDispose((ref) async {
-  await Future.delayed(1.seconds);
+final vehicleStatusProvider =
+    FutureProvider.autoDispose.family<VehicleStatus, String>(
+  (ref, registration) async {
+    await Future.delayed(1.seconds);
 
-  final random = Random.secure();
-  const values = VehicleStatus.values;
+    final random = Random.secure();
+    const values = VehicleStatus.values;
 
-  return values[random.nextInt(values.length - 1)];
-});
+    return values[random.nextInt(values.length - 1)];
+  },
+);
