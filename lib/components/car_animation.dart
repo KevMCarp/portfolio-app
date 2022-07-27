ba75069004ec17a0ab75131b55c344c94ebafe2d
Kevin McDermott-Carpenter <kevin.mcdermott-carpenter@concentechltd.co.uk>
2022-07-27 21:37:09 +0100

    update: add reverse

--- lib/components/car_animation.dart
+++ lib/components/car_animation.dart
@@ -7,11 +7,13 @@ class CarAnimation extends StatelessWidget {
   const CarAnimation({
     required this.isPlaying,
     required this.controller,
+    this.reverse = false,
     this.onInit,
     Key? key,
   }) : super(key: key);
 
   final bool isPlaying;
+  final bool reverse;
   final RiveAnimationController controller;
   final void Function(Artboard artboard)? onInit;
 
@@ -25,6 +27,13 @@ class CarAnimation extends StatelessWidget {
     // final imageStart =
     //     isLandscape ? (mediaQuery.size.width / 2) - imageWidth / 2 : 0.0;
 
+    double _leftPosition(double max) {
+      if (reverse) {
+        return isPlaying ? 0 : -max;
+      }
+      return isPlaying ? max : 0;
+    }
+
     return LayoutBuilder(builder: (context, constraints) {
       final imageWidth = constraints.maxWidth;
 
@@ -35,7 +44,7 @@ class CarAnimation extends StatelessWidget {
           children: [
             AnimatedPositioned(
               duration: isPlaying ? kTransitionDuration : Duration.zero,
-              left: isPlaying ? constraints.maxWidth : 0,
+              left: _leftPosition(constraints.maxWidth),
               child: SizedBox(
                 width: imageWidth,
                 height: imageWidth / 3,
